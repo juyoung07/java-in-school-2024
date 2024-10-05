c288faaf4da88b85ca5b6646011e5122daa6fffb
Yun juyoung <yunju5090@gmail.com>
2024-10-06 06:35:46 +0900

    예외처리... 04  finally 실행..

--- ExceptionEx/src/excep01/ArithException02.java
+++ ExceptionEx/src/excep01/ArithException02.java
@@ -12,16 +12,18 @@ public class ArithException02 {
 			c = a/b;	// 이 문장에서 예외가 발생하면 아래 문장은 실행 X
 			
 			System.out.println(c);
-			for (int i = 0; i <= name.length; i++)
+			for (int i = 0; i < name.length; i++)
 				System.out.println(name[i]);
 			
-		} catch (Exception e) {		// 이 문장은 예외처리의 마지막에 위치해야 함
-			System.out.println("예외발생");		
-			System.out.println(e.toString());
-		} catch (ArithmeticException ar) {
+		} catch (ArithmeticException ar) {	// 예외가 발생하지 않으면 finally 실행
 			System.out.println("0으로 나누었음");
 		} catch (ArrayIndexOutOfBoundsException ai) {
 			System.out.println("배열 범위 벗어난 예외 발생");
-		
+		} catch (Exception e) {
+			System.out.println("예외발생");
+			System.out.println(e.toString());
+		} finally {
+			System.out.println("무조건 실행");
+		}
 	}
 }
