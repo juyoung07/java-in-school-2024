3cd7264608ae500ec9cc751a96b8f12ec387aac8
Yun juyoung <yunju5090@gmail.com>
2024-10-06 06:31:40 +0900

    예외 처리... 02

---
+++ ExceptionEx/src/excep01/ArithException02.java
@@ -0,0 +1,27 @@
+package excep01;
+
+public class ArithException02 {
+	public static void main(String []args) {
+		String[] name = new String[3];
+		name[0] = "윤";
+		name[1] = "주";
+		name[2] = "영";
+		int a = 123, b = 0, c;
+		
+		try {
+			c = a/b;	// 이 문장에서 예외가 발생하면 아래 문장은 실행 X
+			
+			System.out.println(c);
+			for (int i = 0; i <= name.length; i++)
+				System.out.println(name[i]);
+			
+		} catch (ArithmeticException ar) {
+			System.out.println("0으로 나누었음");
+		} catch (ArrayIndexOutOfBoundsException ai) {
+			System.out.println("배열 범위 벗어난 예외 발생");
+		} catch (Exception e) {
+			System.out.println("예외발생");
+			System.out.println(e.toString());
+		}
+	}
+}
