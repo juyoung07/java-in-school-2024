56744ed4b569f921134eca76e334ed6b1b595d46
Yun juyoung <yunju5090@gmail.com>
2024-10-08 00:42:44 +0900

    스레드,, runnable 알아보기..

---
+++ ThreadEx/src/th03/RunnableEx01.java
@@ -0,0 +1,30 @@
+package th03;
+
+class Top extends Thread {
+	public void run() {
+		for (int i = 1; i < 50; i++) {
+			try { Thread.sleep(50); } catch (Exception e) {}
+			if (i % 5 != 0) System.out.println(i + " \t");
+			else System.out.println(i);
+		}
+	}
+}
+
+public class RunnableEx01 {
+	public static void main(String[] args) {
+		System.out.println("프로그램 시작");
+		
+		Top t = new Top();
+		Thread thd = new Thread(t);
+		
+		thd.start();
+		
+		try {
+			Thread.sleep(200);
+		} catch (Exception e) {
+			System.out.println("프로그램 종료");
+		}
+		
+	}
+
+}
