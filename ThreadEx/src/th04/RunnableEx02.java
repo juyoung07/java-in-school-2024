c22aae8f25cf8884ed9d3e653b626c038653a3d7
Yun juyoung <yunju5090@gmail.com>
2024-10-08 00:45:59 +0900

    스레드.. runnable과 다시 만나기..

---
+++ ThreadEx/src/th04/RunnableEx02.java
@@ -0,0 +1,28 @@
+package th04;
+
+class Top extends Thread {
+	public void run() {
+		for (int i = 1; i < 50; i++) {
+			try { Thread.sleep(50); } catch (Exception e) {}
+			if (i % 5 != 0) System.out.println(i + " \t");
+			else System.out.println(i);
+		}
+	}
+}
+
+public class RunnableEx02 {
+	public static void main(String[] args) {
+		System.out.println("프로그램 시작");
+		
+		Top t = new Top();
+		
+		Thread thd1 = new Thread(t);
+		Thread thd2 = new Thread(t);
+		
+		thd1.start();
+		thd2.start();
+		
+		System.out.println("프로그램 종료");
+	}
+
+}
